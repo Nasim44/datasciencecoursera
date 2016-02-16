cc07315f56f7ac0d818c95ba54ee139216ada2c3
Nasim <n_moallemi@yahoo.com>
2016-02-16 12:52:20 -0500

    assignment1 submission

--- explor/plot1.R
+++
@@ -1,7 +0,0 @@
-data <- read.table("power.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-data_subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-G_p <- as.numeric(data_subset$Global_active_power)
-
-png("plot1.png")
-hist(G_p, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-dev.off()
